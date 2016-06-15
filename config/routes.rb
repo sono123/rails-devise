9671ef33cb67aa2af637e30b9eaa1bb8f89361dd
steveono <steveono@gmail.com>
2016-06-14 22:19:04 -0600

    Fix flash messages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  # get '/signin' => 'sessions#new', :as => :signin
+  # get '/signout' => 'sessions#destroy', :as => :signout
+  resources :users
   root 'visitors#index'
 end
